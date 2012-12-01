b14a1f3fdbc6416d9aecf1733e994daf22146c0c
Karl Horky <karl.horky@gmail.com>
2012-12-01 14:04:50 +0100

    add powershell profile

---
+++ Users/Karl/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Set-Alias subl 'C:\Program Files\Sublime Text 2\sublime_text.exe'
+
+function e { subl . }
+function s { ruby script\rails server }
+
+function dev { cd ~\development }
+function cx { cd ~\development\codexistential }
+function ce { cd ~\development\configelation }
+function ms { cd ~\development\ms }
+function profile { cd ~\Documents\WindowsPowerShell }
